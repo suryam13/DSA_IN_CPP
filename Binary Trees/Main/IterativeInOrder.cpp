047c8fcd44219482d1c8832b59adca5d19eb3e61
suryam pandey <suryammpandeyy@gmail.com>
2025-02-02 23:24:49 +0530

    trees

---
+++ Binary Trees/Main/IterativeInOrder.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+// vector<int> inorderTraversal(TreeNode* root) {
+
+//         // LEFT ROOT RIGHT
+//         if (!root)
+//           return {};
+
+//         TreeNode* mover = root;
+//         stack<TreeNode*> st;
+
+//         vector<int> inOrder;
+
+//         // MOVE AS <-- LEFT AS POSSIBLE IF REACHED NULL THEN THAT IS THE ROOT
+//         // PRINT AND GO RIGHT -->
+//         while (true) {
+//             if (mover) // not null
+//             {
+//                 st.push(mover);
+//                 mover = mover->left;
+//             }
+
+//             else {
+//                 if (st.empty())
+//                     break;
+//                 mover = st.top();
+//                 st.pop();
+//                 inOrder.push_back(mover->val);
+//                 mover = mover->right;
+//             }
+//         }
+
+//         return inOrder;
+//     }
+ int main()
+{
+    
+    return 0;
+}
