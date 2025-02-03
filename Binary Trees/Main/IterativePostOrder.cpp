b2ff48b48e828424bf276ca3a7236403604dce4f
suryam pandey <suryammpandeyy@gmail.com>
2025-02-03 22:23:15 +0530

    trees

--- Binary Trees/Main/IterativePostOrder.cpp
+++ Binary Trees/Main/IterativePostOrder.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//Two Stacks
-// vector<int> postorderTraversal(TreeNode *root)
-// {
-//     if (!root)
-//         return {};
-//     // LEFT RIGHT ROOT
-//     vector<int> postOrder;
+// Two Stacks
+//  vector<int> postorderTraversal(TreeNode *root)
+//  {
+//      if (!root)
+//          return {};
+//      // LEFT RIGHT ROOT
+//      vector<int> postOrder;
 
 //     stack<TreeNode *> st1; // Pushing left first then right
 //     stack<TreeNode *> st2; // Storing Top Elements of another
@@ -36,6 +36,31 @@ using namespace std;
 //     }
 //     return postOrder;
 // }
+
+// Method 2:
+/* vector<int> postOrder(Node* node) {
+        vector<int> ans;
+
+        stack<Node*> st;
+        st.push(node);
+
+        while(!st.empty())
+        {
+
+             Node* temp= st.top();
+            st.pop();
+
+
+            if(temp->left)st.push(temp->left);
+            if(temp->right)st.push(temp->right);
+
+            ans.push_back(temp->data);
+
+        }
+
+        reverse(ans.begin(),ans.end());
+        return ans;
+    } */
 int main()
 {
 
