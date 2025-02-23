b43c77a28484eb02139c2a520f16d65c0c43f0ae
suryam pandey <suryammpandeyy@gmail.com>
2025-02-23 21:28:02 +0530

    commit

--- Binary Trees/Main/Traversal Techniques/MorrisInorderTraversal.cpp
+++ Binary Trees/Main/Traversal Techniques/MorrisInorderTraversal.cpp
@@ -20,50 +20,50 @@ S.C : O()
 
 vector<int> inOrder;
 
-void inorderMorris()
-{
-    TreeNode* curr=root; //root of the tree
+// void inorderMorris()
+// {
+//     TreeNode* curr=root; //root of the tree
 
-    while(!curr)
-    {   
-        if(!curr->left) //means NULL
-        {
-            //since curr is the only possible root at this instant
-            inOrder.push_back(curr->val);
-            curr=curr->right;
-        }
+//     while(curr)
+//     {   
+//         if(!curr->left) //means NULL
+//         {
+//             //since curr is the only possible root at this instant
+//             inOrder.push_back(curr->val);
+//             curr=curr->right;
+//         }
 
-        else //means left exists so map or make thread the rightmost in the left part to the root before moving left
-        {   
-            TreeNode* temp=curr->left;
+//         else //means left exists so map or make thread the rightmost in the left part to the root before moving left
+//         {   
+//             TreeNode* temp=curr->left;
 
-            //means both the conditions should be satisfied that is keep moving and temp->right should not be null and the temp->right should not be pointing to curr,if it is means it was been threaded earlier so now delete the thread 
-            while(temp->right && temp->right!=curr)
-            {
-                temp=temp->right;
-            }
+//             //means both the conditions should be satisfied that is keep moving and temp->right should not be null and the temp->right should not be pointing to curr,if it is means it was been threaded earlier so now delete the thread 
+//             while(temp->right && temp->right!=curr)
+//             {
+//                 temp=temp->right;
+//             }
 
-            //if it comes out of the loop there would propably be one reason resulting in to be false;
+//             //if it comes out of the loop there would propably be one reason resulting in to be false;
 
-            if(!temp->right) //means it is NULL means unmapped/unthreaded so make a thread
-            {
-                temp->right=curr;
-                //now curr can easily come to left as mapping is been done
-                curr=curr->left;
-            }
+//             if(!temp->right) //means it is NULL means unmapped/unthreaded so make a thread
+//             {
+//                 temp->right=curr;
+//                 //now curr can easily come to left as mapping is been done
+//                 curr=curr->left;
+//             }
 
-            else //means it is threaded
-            {
-                //since threaded means now the root is reached so push,unthread and then move to right 
-                temp->right=NULL;
-                inOrder.push_back(curr->val);
-                curr=curr->right;
+//             else //means it is threaded
+//             {
+//                 //since threaded means now the root is reached so push,unthread and then move to right 
+//                 temp->right=NULL;
+//                 inOrder.push_back(curr->val);
+//                 curr=curr->right;
 
-            }
-        }
+//             }
+//         }
 
-    }
-}
+//     }
+// }
  int main()
 {
     
