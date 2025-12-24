43fb06ba8a515786838063ef5d5d9ce991284545
suryam pandey <suryammpandeyy@gmail.com>
2025-12-24 15:03:08 +0530

    rev

--- Arrays/Arrays/Easy/CheckIfArrayIsRotatedAndSorted.cpp
+++ Arrays/Arrays/Easy/CheckIfArrayIsRotatedAndSorted.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // LC-1752
-// key observations
+// key observations :
 // the array which has more than one peak element cannot be rotated and sorted
 // normal sorted array would have one peak that would be known by comparing first and last element
 // rotated and sorted would have 1 peak while traversing somehwere mid in the array,the first element would never be less then the last
